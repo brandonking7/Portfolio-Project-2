0fd1f53f9ab3de3583ae9aca32aea7b6f99a3492
brandonking7 <kingrome74@gmail.com>
2017-03-25 19:07:15 -0400

    updated Post model

---
+++ models/post.js
@@ -0,0 +1,7 @@
+var Schema = require('../db/schema');
+var mongoose = require('mongoose');
+
+var Post = Schema.Post;
+
+
+module.exports = Post;
--- models/user.js
+++ models/user.js
@@ -2,4 +2,7 @@ var Schema = require('../db/schema');
 var mongoose = require('mongoose');
 
 var User = Schema.User;
+
+
+
 module.exports = User;
