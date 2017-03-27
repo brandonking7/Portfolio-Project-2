fba177aa38a9923853adee6e9947d7143e7c45c5
brandonking7 <kingrome74@gmail.com>
2017-03-26 23:10:31 -0400

    updated post views and post routes

---
+++ routes/posts.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var Post = require('../models/post');
+
+//Post Index Router
+router.get('/', function indexP)
