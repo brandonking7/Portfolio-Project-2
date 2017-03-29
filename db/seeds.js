68a9b7dff2aa176b14c2ab2d280d912c0b37fb1a
brandonking7 <kingrome74@gmail.com>
2017-03-29 09:43:48 -0400

    first Heroku push

--- db/seeds.js
+++ db/seeds.js
@@ -9,25 +9,25 @@ User.remove({}, function(err) {
     console.log(err);
 });
 
-var girlFriend = new User({
-  first_name: 'Allison',
-  last_name: 'Gilmore',
-  username: 'FEFE!'
-});
-var mom = new User({
-  first_name: 'Vonda',
-  last_name: 'King',
-  username: 'Ma'
-});
-
-girlFriend.save(function(err){
-  if(err) console.log(err);
-
-  console.log('Wife created!')
-});
-
-mom.save(function(err){
-  if(err) console.log(err);
-
-  console.log('Mom created!');
-});
+// var girlFriend = new User({
+//   first_name: 'Allison',
+//   last_name: 'Gilmore',
+//   username: 'FEFE!'
+// });
+// var mom = new User({
+//   first_name: 'Vonda',
+//   last_name: 'King',
+//   username: 'Ma'
+// });
+//
+// girlFriend.save(function(err){
+//   if(err) console.log(err);
+//
+//   console.log('Wife created!')
+// });
+//
+// mom.save(function(err){
+//   if(err) console.log(err);
+//
+//   console.log('Mom created!');
+// });
